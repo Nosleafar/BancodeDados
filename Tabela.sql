0b116804a50a5f07036ffca83c9a9998e75a37cc
Nosleafar <rafaelsonsilva.rs@gmail.com>
2023-09-27 17:35:42 -0300

    banco de dados

---
+++ Tabela.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE cadastro_de_produtos_209068;
+USE cadastro_de_produtos_209068;
+
+insert into produto values(null, "Caneca do Marcos, meu programador", 3.99, 35), (null, "CD Rebeldes, Turnê Brasil", 99.99, 15), (null, "Skate", 300, 02);
